88ce39c96ffcab05d3d4eaef265cbebd154d9a36
FNU Shaliwa <sf452@cornell.edu>
2024-02-27 23:38:33 -0500

    Defined structs for User and Listing

---
+++ MarketplaceApp/Models/Listing.swift
@@ -0,0 +1,14 @@
+//
+//  Listing.swift
+//  MarketplaceApp
+//
+//  Created by Shaliwa on 2/27/24.
+//
+
+import Foundation
+
+struct Listing {
+    let image: String 
+    let title: String
+    let price: String
+}
---
+++ MarketplaceApp/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  MarketplaceApp
+//
+//  Created by Shaliwa on 2/27/24.
+//
+
+import Foundation
+
+struct User {
+    let image: String 
+    let name: String
+    let contact_info: String
+    let email: String
+}
