39302b105c556d17b05d1ca63c7e17bae61b9dc1
FNU Shaliwa <sf452@cornell.edu>
2024-02-27 22:47:47 -0500

    Initial Commit

---
+++ MarketplaceApp/MarketplaceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketplaceAppApp.swift
+//  MarketplaceApp
+//
+//  Created by Cheyenne on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct MarketplaceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
